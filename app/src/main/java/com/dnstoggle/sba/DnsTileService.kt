6761bd35cda42878536f1c64ba94509e5089fa90
th01c <negrusebastian75@gmail.com>
2025-03-18 11:54:20 +0200

    Initial commit

---
+++ app/src/main/java/com/dnstoggle/sba/DnsTileService.kt
@@ -0,0 +1,44 @@
+package com.dnstoggle.sba
+
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import android.content.ContentResolver
+import android.provider.Settings
+import android.util.Log
+import android.graphics.drawable.Icon
+
+class DnsTileService : TileService() {
+
+    override fun onStartListening() {
+        updateTile()
+    }
+
+    override fun onClick() {
+        toggleDns()
+        updateTile()
+    }
+
+    private fun updateTile() {
+        qsTile?.apply {
+            state = if (isDnsEnabled()) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+            label = if (isDnsEnabled()) "dns.adguard-dns.com" else "DNS Off"
+            icon = if (isDnsEnabled()) Icon.createWithResource(this@DnsTileService, R.drawable.ic_dns_on)
+            else Icon.createWithResource(this@DnsTileService, R.drawable.ic_dns_off)
+            updateTile()
+        }
+    }
+
+    private fun toggleDns() {
+        val newDns = if (isDnsEnabled()) "" else "dns.adguard-dns.com"
+        try {
+            Settings.Global.putString(contentResolver, "private_dns_mode", if (newDns.isEmpty()) "off" else "hostname")
+            Settings.Global.putString(contentResolver, "private_dns_specifier", newDns)
+        } catch (e: SecurityException) {
+            Log.e("DnsTileService", "Permission denied: WRITE_SECURE_SETTINGS required")
+        }
+    }
+
+    private fun isDnsEnabled(): Boolean {
+        return Settings.Global.getString(contentResolver, "private_dns_mode") == "hostname"
+    }
+}
